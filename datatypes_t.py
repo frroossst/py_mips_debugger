121b772f7d26ff2a54f354cb381d96ebb3081ad2
Adhyan H <adhyanpatel@gmail.com>
2023-03-27 12:55:23 -0600

    TODO: add test cases to check validity of emulator

---
+++ datatypes_t.py
@@ -0,0 +1,24 @@
+class uint8_t:
+    pass
+
+class int8_t:
+    pass
+
+class uint16_t:
+    pass
+
+class int16_t:
+    pass
+
+class uint32_t:
+    pass
+
+class int32_t:
+    pass
+
+class f32_t:
+    pass
+
+class f64_t:
+    pass
+
