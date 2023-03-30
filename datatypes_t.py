3291dc4b95e2d5dc287be366d6d4825471539392
Adhyan H <adhyanpatel@gmail.com>
2023-03-30 13:27:10 -0600

    further test data types

--- datatypes_t.py
+++ datatypes_t.py
@@ -1,74 +1,85 @@
 from exceptions import InterpreterOverflow, InterpreterUnderflow, InterpreterTypeError
-import struct
 
 class uint8_t:
 
     value = None
 
-    def __init__(self, value) -> None:
+    def check_bounds(self, value):
         if value > 255:
             raise InterpreterOverflow("Value too large for uint8_t")
         if value < 0:
             raise InterpreterUnderflow("Value too small for uint8_t")
 
-        self.value = struct.pack("H", value)
+    def __init__(self, value) -> None:
+        self.check_bounds(value)
+
+        self.value = value
 
     def __repr__(self) -> str:
-        return f"uint8_t({int.from_bytes(self.value, 'little')})"
-
-    def unwrap(self):
-        return int.from_bytes(self.value, 'little')
+        return f"uint8_t({self.value})"
 
     def __add__(self, other):
         if not isinstance(other, uint8_t):
-            raise InterpreterTypeError("unsupported operand type(s) for +: 'uint8_t' and '{}'".format(type(other).__name__))
+            raise InterpreterTypeError(f"unsupported operand type(s) for +: 'uint8_t' and '{type(other).__name__}'")
 
-        curr = int.from_bytes(self.value, 'little')
-        other = int.from_bytes(other.value, 'little')
+        result = self.value + other.value
 
-        result = curr + other
-
-        if result > 255:
-            raise InterpreterOverflow("uint8_t addition resulted in overflow")
+        self.check_bounds(result)
 
         return uint8_t(result)
 
     def __sub__(self, other):
-        sum_num = self.value - other.value
-        if sum_num.bit_length() > 8:
-            raise InterpreterOverflow("Overflow while subtracting uint8_t")
+        if not isinstance(other, uint8_t):
+            raise InterpreterTypeError(f"unsupported operand type(s) for -: 'uint8_t' and '{type(other).__name__}'")
 
-        return uint8_t(sum_num)
+        result = self.value - other.value
 
+        self.check_bounds(result)
+
+        return uint8_t(result)
+
+    def __mul__(self, other):
+        if not isinstance(other, uint8_t):
+            raise InterpreterTypeError(f"unsupported operand type(s) for *: 'uint8_t' and '{type(other).__name__}'")
+
+        result = self.value * other.value
+
+        self.check_bounds(result)
+
+        return uint8_t(result)
+    
+    def __truediv__(self, other):
+        if not isinstance(other, uint8_t):
+            raise InterpreterTypeError(f"unsupported operand type(s) for /: 'uint8_t' and '{type(other).__name__}'")
+
+        result = self.value / other.value
+
+        self.check_bounds(result)
+
+        return uint8_t(result)
+    
+    def __floordiv__(self, other):
+        if not isinstance(other, uint8_t):
+            raise InterpreterTypeError(f"unsupported operand type(s) for //: 'uint8_t' and '{type(other).__name__}'")
+
+        result = self.value // other.value
+
+        self.check_bounds(result)
+
+        return uint8_t(result)
+    
+    def __mod__(self, other):
+        if not isinstance(other, uint8_t):
+            raise InterpreterTypeError(f"unsupported operand type(s) for %: 'uint8_t' and '{type(other).__name__}'")
+
+        result = self.value % other.value
+
+        self.check_bounds(result)
+
+        return uint8_t(result)
+    
 class int8_t:
-
-    def __init__(self, value) -> None:
-        if value > 127:
-            raise InterpreterOverflow("Value too large for int8_t")
-        if value < -128:
-            raise InterpreterUnderflow("Value too small for int8_t")
-
-        self.value = struct.pack("h", value)
-
-    def __repr__(self) -> str:
-        return f"int8_t({int.from_bytes(self.value, 'little')})"
-
-    def unwrap(self):
-        return int.from_bytes(self.value, 'little')
-
-    def __add__(self, other):
-        sum_num = self.value + other.value
-        if sum_num.bit_length() > 8:
-            raise InterpreterOverflow("Overflow while adding int8_t")
-
-        return int8_t(sum_num)
-
-    def __sub__(self, other):
-        sum_num = self.value - other.value
-        if sum_num.bit_length() > 8:
-            raise InterpreterOverflow("Overflow while subtracting int8_t")
-
-        return int8_t(sum_num)
+    pass
 
 class uint16_t:
     pass
@@ -77,42 +88,8 @@ class int16_t:
     pass
 
 class uint32_t:
+    pass
 
-    def __init__(self, value) -> None:
-        if value > 4294967295:
-            raise InterpreterOverflow("Value too large for uint32_t")
-        if value < 0:
-            raise InterpreterUnderflow("Value too small for uint32_t")
-
-        self.value = struct.pack("I", value)
-
-    def __repr__(self) -> str:
-        return f"uint32_t({int.from_bytes(self.value, 'little')})"
-
-    def unwrap(self):
-        return int.from_bytes(self.value, 'little')
-    
-    def __add__(self, other):
-        if not isinstance(other, uint32_t):
-            raise InterpreterTypeError("unsupported operand type(s) for +: 'uint32_t' and '{}'".format(type(other).__name__))
-
-        curr = int.from_bytes(self.value, 'little')
-        other = int.from_bytes(other.value, 'little')
-
-        result = curr+ other
-
-        if result > 4294967295:
-            raise InterpreterOverflow("uint32_t addition resulted in overflow")
-
-        return uint32_t(result)
-    
-    def __sub__(self, other):
-        sum_num = self.value - other.value
-        if sum_num.bit_length() > 32:
-            raise InterpreterOverflow("Overflow while subtracting uint32_t")
-
-        return uint32_t(sum_num)
-    
 class int32_t:
     pass
 
--- unit_tests.py
+++ unit_tests.py
@@ -4,24 +4,16 @@ from exceptions import *
 # testing datatypes_t
 count = 0
 for i in range(0, 256):
-    result = uint8_t(i).unwrap()
+    result = uint8_t(i).value
     expected = i
     assert result == expected, f"Expected {expected}, got {result}"
 
-for i in range(-256, 0):
-    expected = None
-    try:
-        result = int8_t(i).unwrap()
-        assert result == expected, f"Expected {expected}, got {result}"
-    except InterpreterOverflow:
-        pass
-
 result = uint8_t(55) + uint8_t(1)
-expected = uint8_t(56)
-assert result.unwrap() == expected.unwrap(), f"Expected {expected}, got {result}"
+expected = uint8_t(56).value
+assert result.value == expected, f"Expected {expected}, got {result}"
 
 result = uint8_t(55) - uint8_t(1)
-expected = uint8_t(54)
-assert result.unwrap() == expected.unwrap(), f"Expected {expected}, got {result}"
+expected = uint8_t(54).value
+assert result.value == expected, f"Expected {expected}, got {result}"
 
 exit(0)
